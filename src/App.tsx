b894cf1efe1fe84e4f090ef09e8bf8a4a749ea94
duanyang <duanyang.dysjc@bytedance.com>
2021-07-09 23:21:32 +0800

    7.9 登录注册功能实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
 import List from './Pages/List'
 import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 export default function App() {
-  return <Login/>
+  return <Router>
+     <Switch>
+        <Route path='/list' component={List}/>
+        <Route path='/login' component={Login} exact/>
+        <Route path='/register' component={Register} exact/>
+        <Redirect to='/list'/>
+     </Switch>
+  </Router>
 }
--- src/Pages/List/index.tsx
+++ src/Pages/List/index.tsx
@@ -2,7 +2,10 @@ import Search from './Search'
 import Table from './Table'
 import { useState,useEffect } from 'react'
 import qs from 'qs'
+import { UseAuth } from '../../custom-hooks/use-auth'
 import {deleteInvalidParams} from '../../util'
+import { useHistory } from 'react-router'
+import { Redirect } from 'react-router-dom'
 import useDebounce from '../../custom-hooks/use-debounce'
 
 export type User = {
@@ -19,6 +22,11 @@ export interface Project {
 }
 
 export default function(){
+   if (!window.localStorage.getItem('token')) {
+       return <Redirect to={{ pathname:'/login',state:{from:'/list'} }} />
+   }
+   const {push} = useHistory()
+   const {name,logout} = UseAuth()
    const [params,setParams] = useState({personId:0,name:''})
    const [users,setUsers] = useState<User[]>([])
    const [projects,setProjects] = useState<Project[]>([])
@@ -39,5 +47,11 @@ export default function(){
    return <div>
       <Search params={params} setParams={setParams} users={users}/>
       <Table users={users} projects={projects}/>
+      <div style={{ position:'absolute',top:20,right:50 }}> 
+        <span>欢迎{name}归来</span>&nbsp;&nbsp;<a onClick={e => {
+            e.preventDefault()
+            logout().then(_ => push('/login'))
+        }}>退出</a>
+      </div>
    </div>
 }
